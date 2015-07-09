ec0acf155170bddacd664002c252f183bae08efb
guardabrazo <federico.guardabrazo@gmail.com>
2015-07-09 12:12:15 +0200

    Added interactivity and responsive features

--- 03 - Donut chart/js/script.js
+++ 03 - Donut chart/js/script.js
@@ -7,8 +7,8 @@ var dataset = [
 ];
 
 // Dimensions of the chart
-var width = 360;
-var height = 360;
+var width = 500;
+var height = 500;
 var radius = Math.min(width, height)*0.5;
 
 // Color scale
@@ -18,18 +18,42 @@ var radius = Math.min(width, height)*0.5;
 var color = d3.scale.ordinal()
   .range(["#808BFF", "#6995E8", "#73C9FF", "#8069E8", "#B373FF"]);
 
+var tempColor;
+
+// Tooltip
+var tooltip = d3.select("body").append("div")
+            .style("position", "absolute")
+            .style("padding", "0, 10px")
+            .style("background", "white")
+            .style("opacity", 0);
+
+
+
 // Create SVG and g elements
-var svg = d3.select("#chart")
+var svg = d3.select(".chart")
   .append("svg")
-  .attr("width", width)
-  .attr("height", height)
+  .attr("width", "25%")
+  .attr("height", "25%")
+
+  // Making it responsive
+  .attr('viewBox','0 0 '+Math.min(width,height)+' '+Math.min(width,height))
+  .attr('preserveAspectRatio','xMinYMin')
+
   .append("g")
   .attr("transform", "translate(" + (width*0.5) + "," + (height*0.5) + ")");
+
+
+// Text
+var text = svg.append("text")
+  .attr("class", "text")
+  .attr("dy", ".35em")
+  .style("text-anchor", "middle")
+  .text("");
 
 // Set the arc's radius
 var arc = d3.svg.arc()
   .outerRadius(radius)
-  .innerRadius(radius - 40);
+  .innerRadius(radius*0.7);
 
 // Pie layout
 var pie = d3.layout.pie()
@@ -50,10 +74,40 @@ var chart = svg.selectAll("path")
   .attr("fill", function(d, i) {
     return color(d.data.label);
   })
+
+  // Events 
+
+  .on("mouseover", function(d) {
+
+    tooltip.transition()
+      .style("opacity", 0.9);
+
+    text.text(d.value + "%");
+
+    tempColor = this.style.fill;
+    d3.select(this)
+      .style("opacity", 0.5)
+      .style("fill", "yellow");
+  })
+
+  .on("mouseout", function(d) {
+    d3.select(this)
+      .style("opacity", 1)
+      .style("fill", tempColor);
+
+    tooltip.transition()
+      .style("opacity", 0);
+
+    text.text("");
+  })
+
+// Animation
   .transition()
     .duration(1000)
     .attrTween("d", tweenPie)
     .ease();
+
+
 
 
 function tweenPie(b) {
@@ -62,11 +116,13 @@ function tweenPie(b) {
 }
 
 
-// Text
-  svg.append("text")
-        .attr("dy", ".35em")
-        .style("text-anchor", "middle")
-        .text(function(d) { return "Awesome donut"; });
+
+
+
+
+
+
+
 
 
 
